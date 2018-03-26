7aefd030a17e839f32569385c7ac832ded8680a6
zdanowiczkonrad <zdanowicz.konrad@gmail.com>
2018-03-26 12:52:38 +0200

    Added temp fix for the tests with code splitted

--- test/Counter.test.tsx
+++ test/Counter.test.tsx
@@ -3,16 +3,25 @@ import { shallow, mount, render } from 'enzyme';
 import React from 'react';
 import Counter from '@/Counter';
 
-
 describe('Counter', () => {
-    it('Shallow rendering with Enzyme', () => {
+    it('Shallow rendering with Enzyme', (done) => {
         const shallowCounter = shallow(<Counter />);
-        expect(shallowCounter.html()).toBe('<h2>Counter: 0</h2>');
+        setTimeout(() => {
+            expect(shallowCounter.html())
+            .toBe(`<div><h2>Counter: 0</h2><h1>Hi, I am lazy component exported as default</h1></div>`);
+            done();
+        }, 100)
+       
     });
 
-    it('Deep rendering with Enzyme', () => {
+    it('Deep rendering with Enzyme', (done) => {
         const deepRender = mount(<Counter />);
-        expect(deepRender.html()).toBe('<h2>Counter: 0</h2>');
-        deepRender.unmount();
+        setTimeout(() => {
+            expect(deepRender.html())
+            .toBe(`<div><h2>Counter: 0</h2><h1>Hi, I am lazy component exported as default</h1></div>`);
+            deepRender.unmount();
+            done();
+        }, 100);
+      
     });
 });
