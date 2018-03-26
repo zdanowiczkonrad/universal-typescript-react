57aaa550e2db33a4ef8b11858840c2db21478559
zdanowiczkonrad <zdanowicz.konrad@gmail.com>
2018-03-26 12:13:22 +0200

    Added server goal

--- src/component-loader.ts
+++ src/component-loader.ts
@@ -1,7 +1,7 @@
-function createLoader<T>(componentPath: string): () => Promise<T>  {
+export function createLoader<T>(componentPath: string, destructuring?: (module: any) => T): () => Promise<T> {
     return () => new Promise<T>((resolve) => {
         return require.ensure([componentPath], () => {
-          resolve(require(componentPath));
+                return resolve(require(componentPath));
         });
-      });
+    }).then(module => destructuring ?  destructuring(module) : module);
 }
